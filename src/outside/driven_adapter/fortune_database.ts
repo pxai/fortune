f43c5eeb8c48f51b6586ace6c7b0b582dc2d3675
pxai <pello.altadill@gluckgames.com>
2019-12-24 20:16:34 +0100

    Implement get Fortune, better

--- src/outside/driven_adapter/fortune_database.ts
+++ src/outside/driven_adapter/fortune_database.ts
@@ -1,8 +1,19 @@
 import GetFortune from "../../boundary/driven_port/get_fortune";
 
 class FortuneDatabase implements GetFortune {
-    getFortune(lang: string, fortuneType: string): string {
-        throw new Error("Method not implemented.");
+    public getFortune(lang: string, fortuneType: string): string {
+        const fortunes = {  
+            "en" : {
+                "classic": ["Early bird catches the worm", "Another String to your bow"],
+                "jokes": ["Elvis has left the building", "hexagonal is the bee's knees"]
+            },
+            "es": {
+                "classic": ["Early bird catches the worm", "Another String to your bow"],
+                "jokes": ["Elvis has left the building", "hexagonal is the bee's knees"]         
+            }
+        };
+
+        return fortunes[lang][fortuneType][0];
     }
 }
 
